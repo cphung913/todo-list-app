b0d3ec61362497fdd4248a3e0d0db0fe355710b6
Chase Phung <chasephung13@dhcp-v028-155.mobile.uci.edu>
2025-09-28 13:51:28 -0700

    added barebones ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import AddButton from "./components/AddButton";
+import TaskList from "./components/TaskList";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+    <div>
+      <h1 className="text-5xl p-6 pb-8 bg-gray-200">To-Do</h1>
+    </div>
+    <TaskList />
+    <AddButton />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,8 @@
+function TaskList() {
+    return (
+    <div className="h-full">
+        
+    </div>);
+}
+
+export default TaskList;
