4ae75e3be1f8f43285bd6c949e1efddf6812e716
Chase Phung <chasephung13@gmail.com>
2025-09-28 13:51:28 -0700

    added barebones ui

---
+++ src/components/AddButton.tsx
@@ -0,0 +1,5 @@
+function AddButton() {
+    return <button className="bg-blue-500 text-white p-2 rounded absolute right-4 bottom-4" onClick={() => console.log("Click")}>Add Task</button>;
+}
+
+export default AddButton;
