f6ca7a725b01150bbc6ab5c7ddd34804c62098ff
Chase Phung <chasephung13@gmail.com>
2025-09-30 23:27:25 -0700

    added task deletion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,10 @@
 class Task {
     name: string;
     completed: boolean;
-    deleted: boolean;
 
-    constructor(name: string, completed: boolean = false, deleted: boolean = false) {
+    constructor(name: string, completed: boolean = false) {
         this.name = name;
         this.completed = completed;
-        this.deleted = deleted;
     }
 }
 
@@ -19,7 +17,7 @@ function TaskList() {
         <div key={index} data-id={index} className="p-4 border-b border-gray-300 flex items-center h-18">
             <button className={"mx-2 flex justify-center items-center border rounded-full w-6.5 h-6.5 cursor-pointer " + (task.completed ? "bg-green-400 border-green-600" : "bg-gray-100 border-gray-300")} onClick={completeTask}><img src="src/assets/check-solid-full.svg" className={"pointer-events-none h-10/12 " + (task.completed ? "opacity-100" : "opacity-0")}/></button>
             <span className={"ml-4 " + (task.completed ? "line-through italic" : "")}>{task.name}</span>
-            <button className="ml-auto mr-7 h-6 w-6"><img src="./src/assets/trash-solid-full.svg" className="h-full cursor-pointer" /></button>
+            <button className="ml-auto mr-7 h-6 w-6" onClick={deleteTask}><img src="./src/assets/trash-solid-full.svg" className="h-full cursor-pointer pointer-events-none" /></button>
         </div>
        ))}
     </div>);
@@ -31,6 +29,13 @@ function completeTask(e: any) {
     taskList.splice(index, 1);
     temp.completed = !temp.completed;
     temp.completed ? taskList.push(temp) : taskList.unshift(temp);
+    window.location.reload();
+}
+
+function deleteTask(e: any) {
+    const index = e.target.parentElement.dataset.id;
+    taskList.splice(index, 1);
+    window.location.reload();
 }
 
 export function addTask(task: string) {
