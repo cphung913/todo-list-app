e312f513381181c631d8421c5ef604163e79e3cc
Chase Phung <chasephung13@gmail.com>
2025-10-09 23:35:18 -0700

    setup sign in and sign up

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,40 +1,64 @@
-import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css'
 import { auth } from '../firebaseConfig';
-import firebase from 'firebase/compat/app';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 
 function Auth() {
-  let ui = new firebaseui.auth.AuthUI(auth);
 
-  let uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult: any) {
-        console.log("User signed in:", authResult);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-        return false;
-      },
-      uiShown: function() {
-        const loader = document.getElementById('loader');
-        if (loader) loader.style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '/',
-    signInOptions: [{
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: false
-    },
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  };
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const authContainer = document.getElementById('auth-container') as HTMLElement;
+        const screen = document.querySelector('.auth-screen') as HTMLElement;
+        authContainer.classList.remove('scale-100');
+        authContainer.classList.add('scale-0');
+        screen.classList.add('opacity-0');
+        screen.classList.remove('opacity-30');
+        screen.classList.add('pointer-events-none');
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+        
+      });
+  }
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user; 
+        const authContainer = document.getElementById('auth-container') as HTMLElement;
+        const screen = document.querySelector('.auth-screen') as HTMLElement;
+        authContainer.classList.remove('scale-100');
+        authContainer.classList.add('scale-0');
+        screen.classList.add('opacity-0');
+        screen.classList.remove('opacity-30');
+        screen.classList.add('pointer-events-none');
+        // load user data
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  }
 
-  return (<div>
-    <div id="firebaseui-auth-container"></div>
-    <div id="loader">Loading...</div>
-  </div>);
+  return (<>
+    <div id="auth-container" className='z-10 w-xl h-60 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded shadow-lg scale-100 duration-300 flex flex-col gap-4'>
+      <h1 className='mb-2'>Sign Up / Sign In</h1>
+      <input type="email" placeholder='email' value={email} className='border-b-1 focus:outline-0' onChange={(e) => setEmail(e.target.value)}/>
+      <input type="password" placeholder='password' value={password} className='border-b-1 focus:outline-0' onChange={(e) => setPassword(e.target.value)}/>
+      <div className='mt-auto'>
+        <button className='bg-blue-500 rounded p-2 cursor-pointer text-white text-xs' onClick={signUp}>Sign Up</button>
+        <button className='bg-blue-500 rounded p-2 cursor-pointer text-white text-xs ml-4' onClick={signIn}>Sign In</button>
+      </div>
+    </div>
+    <div className="auth-screen absolute inset-0 bg-gray-500 opacity-30 pointer-events-all duration-300">
+    </div>
+  </>);
 }
 
 export default Auth;
